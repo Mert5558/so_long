ef56c3d5874eb375b404d29ebe84ac8c63b9ef3d
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2024-04-19 15:42:50 +0200

    Add files via upload

---
+++ so_long.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 11:39:34 by merdal            #+#    #+#             */
+/*   Updated: 2024/04/19 15:31:05 by merdal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	*ft_memset(void *mem, int value, size_t n)
+{
+	unsigned char	byte_value;
+	unsigned char	*dest;
+	size_t			i;
+
+	byte_value = (unsigned char)value;
+	dest = (unsigned char *)mem;
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = byte_value;
+		i++;
+	}
+	return (mem);
+}
+
+int	main(int argc, char **argv)
+{
+	t_complete	game;
+	
+	if (argc != 2)
+		return (0);
+	ft_memset(&game, 0, sizeof(t_complete));
+	ft_create_map(&game, argv);
+}
